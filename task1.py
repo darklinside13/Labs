34d8cfb508065e40316b97328286fd51cd65315a
darklinside13 <139912572+darklinside13@users.noreply.github.com>
2024-03-14 11:12:36 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,11 +1,22 @@
 import csv
 import sys
 
-data = sys.argv[1]
-row = int(sys.argv[2])
-column = int(sys.argv[3])
+def read_csv_cell(file_path, row_num, col_num):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)
+        try:
+            cell_value = data[row_num][col_num]
+            print(cell_value)
+        except IndexError:
+            print("Ошибка: указанная строка или столбец выходит за пределы таблицы")
 
-with open(data, newline='') as csvfile:
-    csv_table = list(csv.reader(csvfile, delimiter=','))
-    print(csv_table[row - 1][column - 1])
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Использование: python read_csv_cell.py <путь_к_csv_файлу> <номер_строки> <номер_столбца>")
+    else:
+        file_path = sys.argv[1]
+        row_num = int(sys.argv[2])
+        col_num = int(sys.argv[3])
+        read_csv_cell(file_path, row_num, col_num)
 
